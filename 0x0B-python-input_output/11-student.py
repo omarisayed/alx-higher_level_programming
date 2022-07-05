ccacc747d08c0d05fa442a93a95105acb4352031
code vader <osayedunoff@gmail.com>
2022-07-05 10:53:33 +0300

    I/Opy

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,8 +8,22 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         dic1 = {}
-        if hasattr(self, '__dict__'):
+        dic2 = {}
+        if attrs is None:
             return self.__dict__
+        totalargs = len(attrs)
+        if hasattr(self, '__dict__'):
+            dic1 = self.__dict__
+            for key, value in dic1.items():
+                for i in range(totalargs):
+                    if key == attrs[i]:
+                        dic2[key] = value
+            return dic2
         return dic1
+
+    def reload_from_json(self, json):
+        if hasattr(self, '__dict__'):
+            for key, value in json.items():
+                self.__dict__[key] = value
